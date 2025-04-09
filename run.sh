d26395ddf1c4b7a6be67e2e1d8b1d7bacd1acdd3
Nomagno <alvarosaez316@gmail.com>
2025-04-09 16:59:26 +0200

    Remove rlwrap from script, oops

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@ gcc -o fex -g \
         core/forth.c \
         primitives/prims.c \
         primitives/oa.c \
-&& rlwrap sh runhelper.sh forth_libs/bootstrap.fs forth_libs/utilities.fs $@
+&& sh runhelper.sh forth_libs/bootstrap.fs forth_libs/utilities.fs $@
